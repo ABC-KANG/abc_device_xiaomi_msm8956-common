9de28b24d6a0e494423f7cf98c7fddb1864d801d
TheScarastic <warabhishek@gmail.com>
2018-07-26 20:42:53 +0000

    msm8956-common: Symlink WCNSS from /persist
    * Remove local copy of WCNSS_wlan_dictionary.dat
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    
    Co-authored-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -381,8 +381,7 @@ PRODUCT_COPY_FILES += \
 # WCNSS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:system/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
